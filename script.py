8b15c5b01d8681d756bd7bc8c05bbcab1074c885
hayo10 <hayoung10.lim@gmail.com>
2024-09-04 14:26:29 +0900

    script 수정

--- script.py
+++ script.py
@@ -1,20 +1,22 @@
 from datasets import load_dataset
-
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
-#from pipeline_meta import CustomedPipeline
+from transformers import AutoTokenizer, pipeline
+import torch
+import gc
 from customed_pipeline import CustomedPipeline
 from hf import NewPhi3Config
-#from model_meta import CustomedPhi3ForCausalLM
+from safetensors import safe_open
 from model import CustomedPhi3ForCausalLM
-
+import gc
+import os
+import time
 
 torch.random.manual_seed(0)
 model_id = "microsoft/Phi-3-medium-4k-instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
 dataset = load_dataset("allenai/openbookqa", split='validation')
-test = dataset.select(range(5))
+# test = dataset.select(range(20))
 config = NewPhi3Config()
 model = CustomedPhi3ForCausalLM(config)
 pipe = CustomedPipeline(model, config)
@@ -47,12 +49,33 @@ def preprocess(data):
     # return the processed data as a dict
     return {"processed_data": model_inputs}
 
+def batchify(data, batch_size):
+    return [data[i:i + batch_size] for i in range(0, len(data), batch_size)]
+
 #model_inputs = dataset.map(preprocess, batched=True, batch_size=5)
-model_inputs = test.map(preprocess, batched=True, batch_size=5)
+model_inputs = dataset.map(preprocess, batched=True, batch_size=20)
+model_inputs = model_inputs['processed_data']
+messages = [inputs['messages'] for inputs in model_inputs]
+labels = [inputs['answer'] for inputs in model_inputs]
+
+batch_size = 5
+batches = batchify(messages, batch_size)
+tokenized = [tokenizer.apply_chat_template(batch, 
+                                          tokenize=True, 
+                                          padding=True, 
+                                          truncation=True,
+                                          return_tensors="pt", 
+                                          return_dict=True) for batch in batches]
+input_ids = [token['input_ids'] for token in tokenized]
+attention_mask = [token['attention_mask'] for token in tokenized]
+
 
 pipe = CustomedPipeline(model, config)
-inputs = pipe.preprocess(model_inputs)
-print("hi")
-outputs = pipe.forward(inputs)
-result = pipe.postprocess(outputs, inputs['labels'])
+outputs = pipe.forward(input_ids, attention_mask)
+result = pipe.postprocess(outputs, labels)
 print(result)
+
+# for batch in zip(input_ids, attention_mask):
+#     sentence = generate(batch[0], batch[1], 1, 32000)
+#     print(sentence)
+
