71ffbb404d4fbc7ce28f1381479c6acc4881e748
hayo10 <hayoung10.lim@gmail.com>
2024-09-20 15:49:54 +0900

    post 바꿈

--- customed_pipeline.py
+++ customed_pipeline.py
@@ -10,7 +10,7 @@ Original file is located at
 import torch
 from transformers import AutoTokenizer
 from transformers import Phi3Config
-
+import time
 import gc
 gc.collect()
 
@@ -36,64 +36,55 @@ class CustomedPipeline():
         self.tokenizer = AutoTokenizer.from_pretrained(model_id)
         self.model =  model
 
-    # def preprocess(
-    #         self,
-    #         prompt_text,
-    #         prefix="",
-    #         handle_long_generation=None,
-    #         add_special_tokens=None
-    #         ):
 
-    #     inputs = self.tokenizer.apply_chat_template(
-    #             prompt_text,
-    #             add_generation_prompt=True,
-    #             tokenize=True,
-    #             return_tensors="pt",
-    #         return_dict=True,
-    #             padding=True
-    #         ).to('cuda')
-    #     inputs['prompts'] = inputs['input_ids'].shape[-1]
-
-    #     return inputs
 
     def forward(self, input_ids, attention_mask):
-        input_ids = input_ids[0].to('cuda')
-        attention_mask = attention_mask[0].to('cuda')
-        prompt_len = input_ids.shape[1]
+        times = 0
+        result = []
+        
+        for batch in zip(input_ids, attention_mask):
+            inputs = batch[0].to('cuda')
+            masks = batch[1].to('cuda')
+            #prompt_len = batch[0].shape[1]
+        
+            st = time.time()
 
-        generated_sequence = self.model.generate(input_ids=input_ids, attention_mask=attention_mask,max_length=prompt_len+15)
-        return {"generated_sequence": generated_sequence, "prompt_len" :prompt_len}
+            generated_sequence = self.model.generate(input_ids=inputs, attention_mask=masks, max_new_tokens=10 )
+            end = time.time()
+            print('batch inference time ', end - st)
+            result.append(generated_sequence)
+            times += end - st
+        return {"generated_sequence": result}
 
     def find_pattern(self, text):
-        cnt = 0
         idx = []
-        for i in range(len(text)-1,0,-1):
-            if text[i] ==32001 and text[i-1] == 32007:
+        for i in range(91,len(text)-1):
+            if text[i] == 32007 and text[i+1]==32001:
                 idx.append(i)
-                if len(idx) == 2:
-                    break
-        if len(idx) == 2:
-            result = text[idx[1]+1:idx[0]-1]
+           
+        if len(idx) == 0:
+            return text[90:]
+        elif len(idx) == 1:
+            return text[idx[0]+2:]
         else:
-            result = text[idx[0]+1:]
-        return result
+            return text[idx[0]+2: idx[1]]
 
     def postprocess(self,model_outputs, labels, clean_up_tokenization_spaces=True):
-        generated_sequence = model_outputs["generated_sequence"]
 
         result = []
 
         correct = 0
-
-        for i, text in enumerate(generated_sequence):
-            answer = self.find_pattern(text)
-            decoded_answer = self.tokenizer.decode(answer)
-            if decoded_answer == labels[i]:
-                correct += 1
-            result.append([{'generated':decoded_answer, 'label' : labels[i]}])
+        
+        for outputs in model_outputs['generated_sequence']:
+            for i, text in enumerate(outputs):
+                answer = self.find_pattern(text)
+                decoded_answer = self.tokenizer.decode(answer)
+                if decoded_answer == labels[i]:
+                    correct += 1
+                result.append([{'generated':decoded_answer, 'label' : labels[i]}])
             
 
-        print('accuracy : ',correct/len(generated_sequence))
+        print('accuracy : ',correct/len(model_outputs)*len(outputs))
         return result
 
 
