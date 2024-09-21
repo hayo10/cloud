52f083c5ed9a7eb9b9583bc6f55fdb87ca6e4e3d
hayo10 <hayoung10.lim@gmail.com>
2024-09-21 15:53:29 +0900

    pipeline 수정

--- customed_pipeline.py
+++ customed_pipeline.py
@@ -9,10 +9,8 @@ Original file is located at
 
 import torch
 from transformers import AutoTokenizer
-from transformers import Phi3Config
+from datasets import load_dataset
 import time
-import gc
-gc.collect()
 
 torch.random.manual_seed(0)
 model_id = "microsoft/Phi-3-medium-4k-instruct"
@@ -35,25 +33,79 @@ class CustomedPipeline():
         self.config = config
         self.tokenizer = AutoTokenizer.from_pretrained(model_id)
         self.model =  model
+        self.input_ids = []
+        self.attention_mask = []
+        self.device = device
+        self.labels = []
 
-
-
-    def forward(self, input_ids, attention_mask):
+    def batchify(self, data, batch_size):
+        return [data[i:i + batch_size] for i in range(0, len(data), batch_size)]
+    
+    def preprocess(self, data):
+        prefix = "\nRead the question and answer the following sentence in given multiple choice.\nAnswer only the sentence you chose. Never include a question and other word in your answer.\n\nquestion: "
+        model_inputs = []
+        for i in range(len(data['question_stem'])):
+            offset = ord(data['answerKey'][i]) - ord('A')
+            chat_dict = {
+                "messages" :[
+                    {
+                        "role" : "user",
+                        "content" : prefix + data['question_stem'][i] + "\nchoices: ["
+                    }
+                ],
+                "answer" : data['choices'][i]['text'][offset]
+            }
+            
+            for j in range(4):
+                chat_dict['messages'][0]['content'] += "\'" + data['choices'][i]['text'][j]
+                if j < 3:
+                    chat_dict['messages'][0]['content'] += "\', "
+                else:
+                    chat_dict['messages'][0]['content'] += "\']\n"
+            
+            model_inputs.append(chat_dict)
+        
+        # return the processed data as a dict
+        return {"processed_data": model_inputs}
+    
+    def load_data(self, split, batch_size, dataset_name= "allenai/openbookqa", ):
+        dataset = load_dataset(dataset_name, split=split)
+        test = dataset.select(range(40))
+        #model_inputs = dataset.map(self.preprocess, batched=True, batch_size=2)
+        model_inputs = test.map(self.preprocess, batched=True, batch_size=20)
+        model_inputs = model_inputs['processed_data']
+        messages = [inputs['messages'] for inputs in model_inputs]
+        self.labels = [inputs['answer'] for inputs in model_inputs]
+        
+        batch_size = batch_size
+        
+        batches = self.batchify(messages, batch_size)
+        tokenized = [self.tokenizer.apply_chat_template(batch, 
+                                                tokenize=True, 
+                                                padding=True, 
+                                                truncation=True,
+                                                return_tensors="pt", 
+                                                return_dict=True) for batch in batches]
+        
+        self.input_ids = [token['input_ids'] for token in tokenized]
+        self.attention_mask = [token['attention_mask'] for token in tokenized]
+            
+    def forward(self, max_new_tokens):
         times = 0
         result = []
         
-        for batch in zip(input_ids, attention_mask):
-            inputs = batch[0].to('cuda')
-            masks = batch[1].to('cuda')
-            #prompt_len = batch[0].shape[1]
-        
+        for batch in zip(self.input_ids, self.attention_mask):
             st = time.time()
-
-            generated_sequence = self.model.generate(input_ids=inputs, attention_mask=masks, max_new_tokens=10 )
-            end = time.time()
-            print('batch inference time ', end - st)
+            inputs = batch[0].to(self.device)
+            masks = batch[1].to(self.device)
+            #prompt_len = batch[0].shape[1]
+    
+            generated_sequence = self.model.generate(input_ids=inputs, attention_mask=masks, max_new_tokens=max_new_tokens )
             result.append(generated_sequence)
+            end = time.time()
+            print('batch load and inference time ', end - st)
             times += end - st
+        print('total inference time ', times)
         return {"generated_sequence": result}
 
     def find_pattern(self, text):
@@ -69,7 +121,7 @@ class CustomedPipeline():
         else:
             return text[idx[0]+2: idx[1]]
 
-    def postprocess(self,model_outputs, labels, clean_up_tokenization_spaces=True):
+    def postprocess(self,model_outputs, clean_up_tokenization_spaces=True):
 
         result = []
 
@@ -79,12 +131,17 @@ class CustomedPipeline():
             for i, text in enumerate(outputs):
                 answer = self.find_pattern(text)
                 decoded_answer = self.tokenizer.decode(answer)
-                if decoded_answer == labels[i]:
+                if self.labels[i] in decoded_answer:
                     correct += 1
-                result.append([{'generated':decoded_answer, 'label' : labels[i]}])
-            
+                else:
+                    decoded_answer = self.tokenizer.decode(text[91:])
+                    result.append([{'wrong':decoded_answer, 'label' : self.labels[i]}])
+        
+        total = len(model_outputs) * len(outputs)
+        print('맞은 개수', correct)
+        print('총 개수 ',len(self.labels))
 
-        print('accuracy : ',correct/len(model_outputs)*len(outputs))
+        print('accuracy : ',correct/total)
         return result
 
 
